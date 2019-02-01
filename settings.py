d741777a61ed829ab411f494043c125369fb064a
Namgyu Ho <itsnamgyu@gmail.com>
2019-02-01 06:21:37 +0000

    Add gugugu app w/ blank index page

--- settings.py
+++ settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gugugu',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = "/var/www/gugugu.co/static/"
 STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
