7bf4853c87e300ff825565ba840c2f4984231996
Namgyu Ho <itsnamgyu@gmail.com>
2019-02-02 12:34:15 +0000

    Add temp permission changes in refresh script

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,7 +1,9 @@
 # Pull from git, update stuff, deploy Apache!
 git pull;
 source venv/bin/activate;
+sudo chmod g+w db.sqlite3
 python manage.py makemigrations;
 python manage.py migrate;
+sudo chmod g-w db.sqlite3
 python manage.py collectstatic --noinput;
 sudo apachectl restart;
