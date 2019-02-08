f7a89de92dc3699adf9642d50077d5ec905aa3e3
Namgyu Ho <itsnamgyu@gmail.com>
2019-02-08 17:00:14 +0900

    Use static background for mobile

--- gugugu/views.py
+++ gugugu/views.py
@@ -118,6 +118,7 @@ def room_ajax(request, pk):
     }
 
     for message in messages:
+        print(member.name + message.text)
         data['messages'].append({
             'sender': message.member.name,
             'text': message.text,
