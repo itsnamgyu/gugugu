2c4e81800ba90c1956ab1abf34605964498cce49
Namgyu Ho <itsnamgyu@gmail.com>
2019-02-09 01:25:59 +0900

    Change url

--- gugugu/urls.py
+++ gugugu/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('gu/ajax/validate-room-name', views.validate_room_name, name='validate_room_name'),
     path('<slug:name>', views.room, name='room'),
-    path('gu/ajax/room/<int:pk>/ajax', views.room_ajax, name='room_ajax'),
+    path('gu/ajax/room/<int:pk>', views.room_ajax, name='room_ajax'),
 ]
