b97736b586eeb3d5e67837c2bc82cf8715d3d163
Namgyu Ho <itsnamgyu@gmail.com>
2019-02-16 19:25:12 +0900

    Separate background effects to background.js
    Check for mobile devices within background.js

--- gugugu/static/gugugu/js/index.js
+++ gugugu/static/gugugu/js/index.js
@@ -1,14 +1,3 @@
-var granimInstance = new Granim({
-    element: '#canvas-background',
-    direction: 'left-right',
-    isPausedWhenNotInView: true,
-    states : {
-        "default-state": {
-            gradients: [["#501145", "#C63771"], ["#FD804A", "#A70F2B"]]
-        }
-    }
-});
-
 $("#form-content").keydown(function(e){
     if (e.keyCode === 13 && !e.shiftKey && !e.ctrlKey && !e.altKey) {
         e.preventDefault();
