4352194342bd7829ddf54895ca70dc501c547f3e
Namgyu Ho <itsnamgyu@gmail.com>
2019-02-16 21:05:07 +0900

    Add login page + allauth view/url placeholders

--- gugugu/urls.py
+++ gugugu/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from allauth.account.views import LoginView, LogoutView, SignupView
 from . import views
 
 urlpatterns = [
@@ -6,4 +8,9 @@ urlpatterns = [
     path('gu/ajax/validate-room-name', views.validate_room_name, name='validate_room_name'),
     path('<slug:name>', views.room, name='room'),
     path('gu/ajax/room/<int:pk>', views.room_ajax, name='room_ajax'),
+
+    # name field is used in allauth
+    path('gu/login', LoginView.as_view(template_name='gugugu/login.html'), name='account_login'),
+    path('gu/signup', SignupView.as_view(), name='account_signup'),
+    path('gu/logout', LogoutView.as_view(), name='account_logout'),
 ]
