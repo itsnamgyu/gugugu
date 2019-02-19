62792baeb6c885e91595855811b08a1f335aff92
Namgyu Ho <itsnamgyu@gmail.com>
2019-02-19 07:54:47 +0000

    Enable debug mode

--- settings.py
+++ settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '!%q(-)iw@2nx%u&7jcblmv*0r__-uok-(&$#sivyidd!rwcd%z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
