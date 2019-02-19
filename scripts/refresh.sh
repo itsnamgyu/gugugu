020c312461dc37ef3864695ddd9a6995c48d2307
Namgyu Ho <itsnamgyu@gmail.com>
2019-02-19 17:31:18 +0000

    Autoset permissions in refresh script

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -2,6 +2,8 @@
 git pull;
 source venv/bin/activate;
 python manage.py makemigrations;
+sudo chown `whoami` db.sqlite3
 python manage.py migrate;
+sudo chown www-data db.sqlite3
 python manage.py collectstatic --noinput;
 sudo apachectl restart;
--- scripts/update_db.sh
+++ scripts/update_db.sh
@@ -1,4 +1,6 @@
 # Update DB
 source venv/bin/activate;
+sudo chown `whoami` db.sqlite3
 python manage.py makemigrations;
 python manage.py migrate;
+sudo chown www-data db.sqlite3
